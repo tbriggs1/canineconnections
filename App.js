1886a8aa82aedb41c894bfbb34592780ccacee94
tbriggs1 <tombriggs93@protonmail.com>
2021-04-12 20:15:53 +0100

    Updated Event

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import EventList from './components/EventList'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hell I'm Tom</Text>
-      <StatusBar style="auto" />
-    </View>
+    <EventList />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/EventList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom';
+import { Text } from 'react-native';
+
+const EventList = () => {
+    return(
+        <Text>Hello Thomas</Text>
+    )
+}
+
+export default EventList;
