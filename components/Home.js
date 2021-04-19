4c78ff4aba226b3d765eb2851d2091027a916612
tbriggs1 <tombriggs93@protonmail.com>
2021-04-19 21:17:11 +0100

    welcome page

--- components/Home.js
+++ components/Home.js
@@ -7,19 +7,72 @@ const Home = () => {
     return(
         <SafeAreaView> 
             <View>
-                <Text>
-                    HEllo
-                </Text>
+                <StyledImage
+                    source={{
+                    uri: 'https://raw.githubusercontent.com/tbriggs1/alex_prod/main/pets-service-cartoon-landing-page-design-welcome-to-animal-store-concept-cute-dog-cat-pet-hotel-shop-veterinary-clinic-183606739.jpg',
+                    }}
+                />
+                <StyledText>
+                    Welcome to Canine Connections!
+                </StyledText>
+                <StyleText>
+                    Click the button deploy to get started
+                </StyleText>
             </View>
+            <StyledView>
+                <StyledTouchableOpacity
+                        onPress={() => {
+                        navigation.navigate('Homepage', {name: 'Homepage'})
+                    }}
+                    >
+                        <StyledTexter> + Add Dog </StyledTexter>
+                </StyledTouchableOpacity>
+            </StyledView>
         </SafeAreaView>
     )
 }
 
 const StyledImage = styled.Image`
-    height: 150;
-    width: 150;
+    height: 250px;
+    width: 300px;
     margin-left:auto;
+    margin-top: 120px;
     margin-right: auto;
+    border-radius: 15px;
+`
+
+const StyledText = styled.Text`
+    font-size: 24px;
+    text-align: center;
+    margin-top: 50px;
+`
+
+const StyleText = styled.Text`
+    font-size: 18px;
+    text-align: center;
+    margin-top: 50px;
+`
+
+const StyledTouchableOpacity = styled.TouchableOpacity`
+  background: white;
+  border: 1px solid #894EFF;
+  padding: 9px;
+  border-radius: 20px;
+  margin-top: 10px;
+  width: 120px;
+`
+
+const StyledTexter = styled.Text`
+  color: #894EFF;
+  font-size: 16px;
+  text-align: center;
+`
+
+const StyledView = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
 `
 
 
