7ed6fb84183d4bbcf0ed1101eca2438fbfcfc7d6
tbriggs1 <tombriggs93@protonmail.com>
2021-04-27 20:20:32 +0100

    added search and go lucky

--- components/SearchPage.js
+++ components/SearchPage.js
@@ -8,6 +8,10 @@ const SearchPage = () => {
     return(
          <ScrollView>
             <StyledView>
+                <ButtonView>
+                    <StyledButton> Search </StyledButton>
+                    <StyledButton> Go Lucky </StyledButton>
+                </ButtonView>
                 <SearchForm />
                 <SearchResults />
             </StyledView>
@@ -24,3 +28,22 @@ const StyledView = styled.View`
   margin-top: 200px;
 `
 
+const ButtonView = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
+
+const StyledButton = styled.Text`
+    font-size: 10px;
+    margin-left: 10px;
+    padding: 5px;
+    padding-top: 7px;
+    padding-bottom: 7px;
+    color: white;
+    background: blue;
+    border-radius: 5px;
+    width: 100px;
+    text-align: center;
+`
